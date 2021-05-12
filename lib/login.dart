f04da254e04fa94b313927e72a4937e5aa0b3882
Brendan Horst <yoshistar2000@gmail.com>
2021-05-12 01:50:50 -0400

    Created login.dart

---
+++ lib/login.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+        home: MyApp(),
+      ));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
