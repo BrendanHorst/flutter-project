dd16e4ec8792a7445e852015699bd08588bf5624
Brendan Horst <yoshistar2000@gmail.com>
2021-05-12 05:33:25 -0400

    Added the table to the patient home page

--- lib/patientHome.dart
+++ lib/patientHome.dart
@@ -50,13 +50,273 @@ class PatientHome extends StatelessWidget {
             ListTile(
               title: Text("Roster"),
             ),
-            ListTile(
-              title: Text("Profile"),
-            )
 
           ],
         )
       ),
+
+      body: Center(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  color: Colors.red,
+                  height: 30,
+                  width: 100,
+                  margin: EdgeInsets.only(top: 20),
+                  child: Text("Patient ID",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    )),
+                ),
+                Container(
+                  height: 30,
+                  width: 100,
+                  margin: EdgeInsets.only(top: 20),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.red),
+                  )
+                )
+              ]
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    color: Colors.red,
+                    height: 30,
+                    width: 100,
+                    margin: EdgeInsets.only(top: 20),
+                    child: Text("Patient Name",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        )),
+                  ),
+                  Container(
+                      height: 30,
+                      width: 100,
+                      margin: EdgeInsets.only(top: 20),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.red),
+                      )
+                  )
+                ]
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    color: Colors.red,
+                    height: 30,
+                    width: 100,
+                    margin: EdgeInsets.only(top: 20),
+                    child: Text("Date",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        )),
+                  ),
+                  Container(
+                      height: 30,
+                      width: 100,
+                      margin: EdgeInsets.only(top: 20),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.red),
+                      )
+                  )
+                ]
+            ),
+
+            Container(
+              width: 300,
+              margin: EdgeInsets.only(top: 40),
+              child: Column(
+                children: [
+                  Container(
+                    color: Colors.grey,
+                    height: 30,
+                    padding: EdgeInsets.all(5),
+                    margin: EdgeInsets.only(top: 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          child: Text("Doctor's Name")
+                        ),
+                        Container(
+                          color: Colors.white,
+                          width: 100,
+                          height: 20,
+                        )
+                      ],
+                    )
+                  ),
+                  Container(
+                      color: Colors.grey,
+                      height: 30,
+                      padding: EdgeInsets.all(5),
+                      margin: EdgeInsets.only(top: 5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                              child: Text("Doctor's Appointment")
+                          ),
+                          Container(
+                            color: Colors.white,
+                            width: 100,
+                            height: 20,
+                          )
+                        ],
+                      )
+                  ),
+                  Container(
+                      color: Colors.grey,
+                      height: 30,
+                      padding: EdgeInsets.all(5),
+                      margin: EdgeInsets.only(top: 5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                              child: Text("Caregiver's Name")
+                          ),
+                          Container(
+                            color: Colors.white,
+                            width: 100,
+                            height: 20,
+
+                          )
+                        ],
+                      )
+                  ),
+                  Container(
+                      color: Colors.grey,
+                      height: 30,
+                      padding: EdgeInsets.all(5),
+                      margin: EdgeInsets.only(top: 5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                              child: Text("Morning Medicine")
+                          ),
+                          Container(
+                            color: Colors.white,
+                            width: 100,
+                            height: 20,
+                          )
+                        ],
+                      )
+                  ),
+                  Container(
+                      color: Colors.grey,
+                      height: 30,
+                      padding: EdgeInsets.all(5),
+                      margin: EdgeInsets.only(top: 5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                              child: Text("Afternoon Medicine")
+                          ),
+                          Container(
+                            color: Colors.white,
+                            width: 100,
+                            height: 20,
+                          )
+                        ],
+                      )
+                  ),
+                  Container(
+                      color: Colors.grey,
+                      height: 30,
+                      padding: EdgeInsets.all(5),
+                      margin: EdgeInsets.only(top: 5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                              child: Text("Night Medicine")
+                          ),
+                          Container(
+                            color: Colors.white,
+                            width: 100,
+                            height: 20,
+                          )
+                        ],
+                      )
+                  ),
+                  Container(
+                      color: Colors.grey,
+                      height: 30,
+                      padding: EdgeInsets.all(5),
+                      margin: EdgeInsets.only(top: 5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                              child: Text("Breakfast")
+                          ),
+                          Container(
+                            color: Colors.white,
+                            width: 100,
+                            height: 20,
+                          )
+                        ],
+                      )
+                  ),
+                  Container(
+                      color: Colors.grey,
+                      height: 30,
+                      padding: EdgeInsets.all(5),
+                      margin: EdgeInsets.only(top: 5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                              child: Text("Lunch")
+                          ),
+                          Container(
+                            color: Colors.white,
+                            width: 100,
+                            height: 20,
+                          )
+                        ],
+                      )
+                  ),
+                  Container(
+                      color: Colors.grey,
+                      height: 30,
+                      padding: EdgeInsets.all(5),
+                      margin: EdgeInsets.only(top: 5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                              child: Text("Dinner")
+                          ),
+                          Container(
+                            color: Colors.white,
+                            width: 100,
+                            height: 20,
+                          )
+                        ],
+                      )
+                  )
+                ],
+              )
+            )
+          ],
+        )
+      )
     );
   }
 }
