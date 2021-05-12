905955f0b2debe90122ef0f439f261cbeceeb042
Brendan Horst <yoshistar2000@gmail.com>
2021-05-12 02:19:26 -0400

    Made the layout for the login page

--- lib/login.dart
+++ lib/login.dart
@@ -11,6 +11,72 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(10),
+          height: 300,
+          width: 300,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            border: Border.all(color: Colors.black),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                child: Text(
+                    "Sign In",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  )
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Username',
+                  ),
+                )
+              ),
+              Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  )
+              ),
+              GestureDetector(
+                onTap: () {
+                  print("test");
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 20,
+                    width: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Text("Submit"),
+                )
+              )
+            ],
+          )
+        )
+      )
     );
   }
 }
